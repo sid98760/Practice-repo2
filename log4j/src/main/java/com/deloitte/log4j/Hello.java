7276919a036a0ff76547485c286a5d145f023ab9
sid98760 <siddheshchinchole32@gmail.com>
2024-11-14 12:54:08 +0530

    Adding log4j

---
+++ log4j/src/main/java/com/deloitte/log4j/Hello.java
@@ -0,0 +1,21 @@
+package com.deloitte.log4j;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		Logger log = LogManager.getLogger(Hello.class.getName());
+		log.fatal("Fatal Message");
+		log.error("Error Message");
+		log.warn("Warn Message");
+		log.info("Info Message");
+		log.debug("Debug Message");
+		log.trace("Trace Message");
+
+	}
+
+}
